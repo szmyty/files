7432491a8aab4644ac7bd80c91cf9e568579147a
Alan Szmyt <szmyty@gmail.com>
2024-04-25 09:22:25 -0400

    Create docker-entrypoint.sh

---
+++ caddy/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+
+# Substitute environment variables in Caddyfile.template
+# shellcheck disable=SC2016
+envsubst '$GEOIP_ACCOUNT_ID,$GEOIP_LICENSE_KEY' < "$CADDYFILE" > "$CADDYFILE.tmp" && mv "$CADDYFILE.tmp" "$CADDYFILE"
+
+# Runs Caddy and blocks indefinitely; i.e. "daemon" mode.
+# https://caddyserver.com/docs/command-line#caddy-run
+"$CADDY" run --config "$CADDYFILE" --adapter caddyfile
