6a56f7e5ec8b2803b168ef2cac529b094e335a4d
Alan Szmyt <szmyty@gmail.com>
2024-11-10 14:14:24 -0500

    Update.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 # terraform/provider.tf
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
   credentials = file("<PATH_TO_YOUR_SERVICE_ACCOUNT_KEY>.json")
 }
