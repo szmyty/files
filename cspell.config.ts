ce7ce9df1019a45c0c285baeafb9b73a340d5f63
Alan Szmyt <szmyty@gmail.com>
2024-11-18 18:41:28 -0500

    Update.

--- cspell.config.ts
+++ cspell.config.ts
@@ -6,3 +6,50 @@
  *
  * @type {import("cspell").CSpellUserSettings}
  */
+import { noCase } from "change-case";
+import { type AdvancedCSpellSettings } from "@cspell/cspell-types";
+import { type PackageManifest } from "@pnpm/types";
+import { type PackageJson } from "type-fest";
+import { readPackageJson } from "@pnpm/read-package-json";
+
+const packageJson: PackageManifest = readPackageJson("package.json");
+
+console.log(noCase(packageJson.name, { delimiter: "-" }));
+
+const settings: AdvancedCSpellSettings = {
+  version: "0.2",
+  language: "en-US",
+  useGitignore: true,
+  ignorePaths: [
+    "**/.gitignore",
+    "**/pnpm-lock.yaml",
+    "**/package.json",
+    "**/package-lock.json",
+    "**/node_modules",
+    "**/dist",
+    "**/build",
+    "**/coverage",
+    "**/public",
+    "**/venv",
+    "**/virtualenv",
+    "**/.venv",
+    "**/temp",
+    "**/tmp",
+    "**/out",
+    "**/output",
+    "**/bin",
+    "**/.git",
+    "**/.vscode",
+    "**/.idea",
+    "**/*.drawio",
+    "**/*.svg",
+    "**/*.png",
+    "**/*.jpg",
+    "**/*.jpeg",
+    "**/*.otf",
+    "**/.vitepress/cache",
+    "**/.vitepress/dist",
+  ]
+};
+
+export default settings;
